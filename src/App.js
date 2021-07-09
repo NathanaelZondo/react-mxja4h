884a376aa7299ff88793904bbd1809ba2b254fdb
NathanaelZondo <fumanizondo@gmail.com>
2021-07-09 19:56:10 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import CallIcon from '@material-ui/icons/Call';
+import EmailIcon from '@material-ui/icons/Email';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    padding:4,
+    width:"90%",
+    alignItems:"center",
+    justifyContent:"center"
   },
   paper: {
     padding: theme.spacing(2),
@@ -19,41 +31,60 @@ export default function FullWidthGrid() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    
+<div align="center">
+    <div className={classes.root} align="center">
+      
       <Grid container spacing={3}>
+        <Paper>
         <Grid item xs={12}>
-        <div className={classes.root}>
+        <div className={classes.root} style={{margin:"5px"}}>
       <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>
+        <Grid item xs={4}>
+          <Paper className={classes.paper} style={{height:"170px",backgroundSize:"cover", 
+      backgroundImage: `url("https://images.pexels.com/photos/450212/pexels-photo-450212.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")` 
+    }}>
         
-<img src="https://images.pexels.com/photos/450212/pexels-photo-450212.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" width="300" height="300" style={{"box-shadow": "-3px 1px 22px 9px rgba(0,0,0,0.27)"}}/>
+
 
 
       
           </Paper>
         </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+        <Grid item xs container
+  direction="row"
+  justifyContent="flex-end"
+  alignItems="baseline">
+          
+          <Grid item xs>
+          <p>I regard myself as a person who is fascinated by challenges in all sorts of way, work or life, that is merely based on my belief that evolving and adapting to challenges is the whole concept of being competitive and that is what i want to bring to the workplace. With the less experience that i have only means i have more room for improvement especially in programming languages, I have taught myself a few and am willing to learn and be productive as a software developer.</p>
+          
         </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+        <Grid item xs={1}>
+          <div style={{width:"5px"}}>
+        <FacebookIcon />
+        <TwitterIcon  />
+        <CallIcon />
+        <EmailIcon  />
+        <InstagramIcon  />
+          </div>
         </Grid>
+
+
+
+        </Grid>
+       
       </Grid>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
-        </Grid>
-    
-      </Grid>
+  
     </div>
 
 
           
         </Grid>
+      </Paper>
       
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>
+        <Grid item  xs={12} sm={6}>
+          <Paper className={classes.paper} elevation={0}>
          <h2 style={{color:"black"}}> EDUCATION</h2>
          <Divider />
          <p>TERTIARY</p>
@@ -72,8 +103,8 @@ export default function FullWidthGrid() {
 
           </Paper>
         </Grid>
-        <Grid item  sm={4}>
-          <Paper className={classes.paper}> 
+        <Grid item  xs={12} sm={6}>
+          <Paper className={classes.paper} elevation={0}> 
           <h2 style={{color:"black"}}> ATTRIBUTES</h2>
           <Divider />
 <p>
@@ -81,12 +112,14 @@ I regard myself as a person who is fascinated by challenges in all sorts of way,
 
 </Paper>
         </Grid>
-        <Grid item sm={5} >
-          <Paper className={classes.paper}>
+        <Grid  >
+        <div>
+          <Paper className={classes.paper} style={{width:"100vh"}}>
          <h2 style={{color:"black"}}> WORK EXPERIENCE</h2>
          <Divider />
-          <p>SEASONAL SALES ASSISTANT</p>
-<p>Typo Vaal Mall</p>
+         
+          <p>SEASONAL SALES ggggggggggggggfffffffffffffffffffffffffffffffffff</p>
+<p>Typo Vaal Mallft4444444ft44444444444444tgggggggggggggggggggggggg4ttttttttttg4tttttttttttg6</p>
 <p>November 2020 to February 2021</p>
 
 <p>SEASONAL SALES ASSISTANT</p>
@@ -94,9 +127,11 @@ I regard myself as a person who is fascinated by challenges in all sorts of way,
 <p>November 2019 to January 2020</p>
 
           </Paper>
+          </div>
         </Grid>
     
       </Grid>
+    </div>
     </div>
   );
 }
