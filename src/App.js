4db5a1dbe1a926345ec88761bd280e10f15c8fbd
NathanaelZondo <fumanizondo@gmail.com>
2021-07-09 18:18:44 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,31 +22,49 @@ export default function FullWidthGrid() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          
+        <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs>
           <Paper className={classes.paper}>
-          PERSONAL ATTRIBUTES
-
-I regard myself as a person who is fascinated by challenges in all sorts of way, work or life, that is merely based on my belief that evolving and adapting to challenges is the whole concept of being competitive and that is what i want to bring to the workplace. With the less experience that i have only means i have more room for improvement especially in programming languages, I have taught myself a few and am willing to learn and be productive as a software developer.
-
+        
+<img src="https://images.pexels.com/photos/450212/pexels-photo-450212.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" width="300" height="300" style={{"box-shadow": "-3px 1px 22px 9px rgba(0,0,0,0.27)"}}/>
 
 
-
+      
           </Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+    
+      </Grid>
+    </div>
+
+
+          
         </Grid>
       
         <Grid item xs={6} sm={3}>
           <Paper className={classes.paper}>
-         <h1> EDUCATION</h1>
-
+         <h2 style={{color:"black"}}> EDUCATION</h2>
+         <Divider />
          <p>TERTIARY</p>
 
-         <p>Institution     : TSHWANE UNIVERSITY OF TECHNOLOGY</p>
+         <p>Institution: TSHWANE UNIVERSITY OF TECHNOLOGY</p>
 
-         <p>Course       : NDip: information Technology: Software development</p>
+         <p>Course: NDip: information Technology: Software development</p>
 
-         <p>Faculty       : Information & Communication Technology(ICT)</p>
+         <p>Faculty: Information & Communication Technology(ICT)</p>
 
-         <p>Level        : final level</p>
+         <p>Level: final level</p>
 
 <h2>HIGH SCHOOL</h2>
 
@@ -54,20 +73,18 @@ I regard myself as a person who is fascinated by challenges in all sorts of way,
           </Paper>
         </Grid>
         <Grid item  sm={4}>
-          <Paper className={classes.paper}>  PERSONAL ATTRIBUTES
+          <Paper className={classes.paper}> 
+          <h2 style={{color:"black"}}> ATTRIBUTES</h2>
+          <Divider />
 <p>
 I regard myself as a person who is fascinated by challenges in all sorts of way, work or life, that is merely based on my belief that evolving and adapting to challenges is the whole concept of being competitive and that is what i want to bring to the workplace. With the less experience that i have only means i have more room for improvement especially in programming languages, I have taught myself a few and am willing to learn and be productive as a software developer.</p>
-
-<p>
-I regard myself as a person who is fascinated by challenges in all sorts of way, work or life, that is merely based on my belief that evolving and adapting to challenges is the whole concept of being competitive and that is what i want to bring to the workplace. With the less experience that i have only means i have more room for improvement especially in programming languages, I have taught myself a few and am willing to learn and be productive as a software developer.</p>
-
 
 </Paper>
         </Grid>
         <Grid item sm={5} >
           <Paper className={classes.paper}>
-         <h1> WORK EXPERIENCE</h1>
-
+         <h2 style={{color:"black"}}> WORK EXPERIENCE</h2>
+         <Divider />
           <p>SEASONAL SALES ASSISTANT</p>
 <p>Typo Vaal Mall</p>
 <p>November 2020 to February 2021</p>
